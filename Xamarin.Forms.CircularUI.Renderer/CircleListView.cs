1eef1089499f2200feb9db2b43086b082ca7126a
pius.lee <pius.lee@samsung.com>
2017-11-29 18:36:46 +0900

    Change new renderer for CircleListView.
    Now CircleListView add top and bottom padding automatically.

--- Xamarin.Forms.CircularUI.Renderer/CircleListView.cs
+++ Xamarin.Forms.CircularUI.Renderer/CircleListView.cs
@@ -1,33 +1,327 @@
+﻿using ElmSharp.Wearable;
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
-using Xamarin.Forms.Internals;
+using System.Linq;
+using System.Text;
 using ElmSharp;
-using ElmSharp.Wearable;
+using System.Collections;
 using Xamarin.Forms.Platform.Tizen;
+using Xamarin.Forms.Internals;
+using System.ComponentModel;
 
 namespace Xamarin.Forms.CircularUI.Renderer
 {
-    public class CircleListView : Xamarin.Forms.Platform.Tizen.Native.ListView, IRotaryActionWidget
+    using GroupList = TemplatedItemsList<ItemsView<Cell>, Cell>;
+
+    public class CircleListView : CircleGenList
     {
-        public CircleListView(EvasObject parent, CircleSurface surface) : base()
+        readonly Dictionary<Cell, GenListItem> _itemContexts = new Dictionary<Cell, GenListItem>();
+
+        VisualElement _header;
+        VisualElement _footer;
+
+        public CircleListView(EvasObject parent, CircleSurface surface) : base(parent, surface)
         {
-            CircleSurface = surface;
-            Realize(parent);
+        }
+        public VisualElement Header
+        {
+            get => _header;
+            set
+            {
+                if (_header == value) return;
+                _header = value;
+                UpdateHeader();
+            }
+        }
+        public VisualElement Footer
+        {
+            get => _footer;
+            set
+            {
+                if (_footer == value) return;
+                _footer = value;
+                UpdateFooter();
+            }
         }
 
-        public CircleGenList CircleGenList { get; private set; }
-
-        public IntPtr CircleHandle => CircleGenList.CircleHandle;
-
-        public CircleSurface CircleSurface { get; private set; }
-
-        protected override IntPtr CreateHandle(EvasObject parent)
+        public void ResetGroup(GroupList group)
         {
-            CircleGenList = new CircleGenList(parent, CircleSurface);
-            RealHandle = CircleGenList.RealHandle;
-            return CircleGenList.Handle;
+            GenListItem groupItem;
+            if (_itemContexts.TryGetValue(group.HeaderContent, out groupItem))
+            {
+                groupItem.ClearSubitems();
+            }
         }
+
+        public void AddSource(IEnumerable source)
+        {
+            foreach (var data in source)
+            {
+                var groupList = data as GroupList;
+                if (groupList != null)
+                {
+                    AddGroup(groupList);
+                    foreach (var item in groupList)
+                    {
+                        AddItem(item as Cell, groupList);
+                    }
+                }
+                else
+                {
+                    AddItem(data as Cell, null);
+                }
+            }
+        }
+        public void AddSource(IEnumerable source, Cell before)
+        {
+            foreach (var data in source)
+            {
+                var groupList = data as GroupList;
+                if (groupList != null)
+                {
+                    InsertGroup(groupList, before);
+                    foreach (var item in groupList)
+                    {
+                        AddItem(item as Cell, groupList);
+                    }
+                }
+                else
+                {
+                    InsertItem(data as Cell, before, null);
+                }
+            }
+        }
+        public void AddSource(IEnumerable source, GroupList group, Cell before)
+        {
+            foreach (var data in source)
+            {
+                InsertItem(data as Cell, before, group);
+            }
+        }
+        public void AddSource(IEnumerable source, GroupList group)
+        {
+            foreach (var data in source)
+            {
+                AddItem(data as Cell, group);
+            }
+        }
+
+        public void RemoveSource(IEnumerable source)
+        {
+            foreach (var data in source)
+            {
+                if (data is GroupList)
+                    Remove(data as GroupList);
+                else
+                    Remove(data as Cell);
+            }
+        }
+
+        public void ApplyScrollTo(Cell cell, ScrollToPosition position, bool animated)
+        {
+            GenListItem item;
+            if (_itemContexts.TryGetValue(cell, out item))
+            {
+                ScrollTo(item, position.ToNative(), animated);
+            }
+        }
+        public void ApplySelectedItem(Cell cell)
+        {
+            GenListItem item;
+            if (_itemContexts.TryGetValue(cell, out item))
+            {
+                item.IsSelected = true;
+            }
+        }
+        public void AddItem(Cell cell, GroupList group)
+        {
+            CellRenderer renderer = ListViewCache.Get(cell);
+            GenListItem groupItem = group == null ? null : _itemContexts.GetValueOrDefault(group.HeaderContent);
+            var item = InsertBefore(renderer.Class, new ListViewItemContext(cell, group), LastItem, GenListItemType.Normal, groupItem);
+            RegisterItem(cell, item);
+        }
+        public void InsertItem(Cell cell, Cell before, GroupList group)
+        {
+            CellRenderer renderer = ListViewCache.Get(cell);
+            GenListItem groupItem = group == null ? null : _itemContexts.GetValueOrDefault(group.HeaderContent);
+            GenListItem beforeItem = _itemContexts.GetValueOrDefault(before);
+            var item = InsertBefore(renderer.Class, new ListViewItemContext(cell, group), beforeItem, GenListItemType.Normal, groupItem);
+            RegisterItem(cell, item);
+        }
+        public void AddGroup(GroupList group)
+        {
+            CellRenderer renderer = ListViewCache.Get(group.HeaderContent);
+            var item = InsertBefore(renderer.Class, new ListViewItemContext(group), LastItem, GenListItemType.Group);
+            RegisterItem(group.HeaderContent, item, true);
+        }
+        public void InsertGroup(GroupList group, Cell before)
+        {
+            CellRenderer renderer = ListViewCache.Get(group.HeaderContent);
+            GenListItem beforeItem = _itemContexts.GetValueOrDefault(before);
+            var item = InsertBefore(renderer.Class, new ListViewItemContext(group), beforeItem, GenListItemType.Group);
+            RegisterItem(group.HeaderContent, item, true);
+        }
+        public void Remove(Cell cell)
+        {
+            GenListItem item;
+            if (_itemContexts.TryGetValue(cell, out item))
+            {
+                item.Delete();
+            }
+        }
+        public void Remove(GroupList group)
+        {
+            Remove(group.HeaderContent);
+            foreach (var data in group)
+            {
+                Remove(data as Cell);
+            }
+        }
+
+        public new void Clear()
+        {
+            base.Clear();
+            Initialize();
+        }
+
+        protected override void OnRealized()
+        {
+            base.OnRealized();
+            Initialize();
+        }
+        void Initialize()
+        {
+            UpdateHeader();
+            UpdateFooter();
+        }
+        void UpdateHeader()
+        {
+            GenItemClass cls = Header == null ? ListViewCache.PaddingItemClass : ListViewCache.InformalItemClass;
+            if (FirstItem == null)
+            {
+                Append(cls, new TypedItemContext(Header, ItemType.TopPadding));
+            }
+            else
+            {
+                var ctx = FirstItem.Data as TypedItemContext;
+                if (ctx != null && (ctx.Type == ItemType.TopPadding || ctx.Type == ItemType.Header))
+                {
+                    FirstItem.UpdateItemClass(cls, new TypedItemContext(Header, Header == null ? ItemType.TopPadding : ItemType.Header));
+                }
+                else
+                {
+                    InsertBefore(cls, new TypedItemContext(Header, Header == null ? ItemType.TopPadding : ItemType.Header), FirstItem);
+                }
+            }
+        }
+        void UpdateFooter()
+        {
+            GenItemClass cls = Footer == null ? ListViewCache.PaddingItemClass : ListViewCache.InformalItemClass;
+            if (LastItem == null)
+            {
+                Append(cls, new TypedItemContext(Footer, ItemType.BottomPadding));
+            }
+            else
+            {
+                var ctx = LastItem.Data as TypedItemContext;
+                if (ctx != null && (ctx.Type == ItemType.BottomPadding || ctx.Type == ItemType.Footer))
+                {
+                    LastItem.UpdateItemClass(cls, new TypedItemContext(Footer, Footer == null ? ItemType.BottomPadding : ItemType.Footer));
+                }
+                else
+                {
+                    Append(cls, new TypedItemContext(Footer, Footer == null ? ItemType.BottomPadding : ItemType.Footer));
+                }
+            }
+        }
+        void RegisterItem(Cell cell, GenListItem item, bool IsGroup = false)
+        {
+            item.SelectionMode = IsGroup ? GenItemSelectionMode.None : GenItemSelectionMode.Always;
+            item.IsEnabled = cell.IsEnabled;
+            item.Deleted += ItemDeletedHandler;
+            _itemContexts[cell] = item;
+
+            if (!IsGroup)
+            {
+                cell.PropertyChanged += OnCellPropertyChanged;
+                (cell as ICellController).ForceUpdateSizeRequested += OnForceUpdateSizeRequested;
+            }
+        }
+        void OnCellPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            var cell = sender as Cell;
+            var renderer = ListViewCache.Get(cell);
+            GenListItem item;
+            if (_itemContexts.TryGetValue(cell, out item))
+            {
+                renderer.SendCellPropertyChanged(cell, item, e.PropertyName);
+            }
+        }
+        void OnForceUpdateSizeRequested(object sender, EventArgs e)
+        {
+            var cell = sender as Cell;
+            GenListItem item;
+            if (_itemContexts.TryGetValue(cell, out item))
+            {
+                item.Update();
+            }
+        }
+        void ItemDeletedHandler(object sender, EventArgs e)
+        {
+            var context = (sender as GenListItem)?.Data as ListViewItemContext;
+            if (context != null)
+            {
+                Cell cell;
+                if (!context.IsGroupItem)
+                {
+                    cell = context.Cell;
+                    cell.PropertyChanged -= OnCellPropertyChanged;
+                    (cell as ICellController).ForceUpdateSizeRequested -= OnForceUpdateSizeRequested;
+                }
+                else
+                {
+                    cell = context.Group.HeaderContent;
+                }
+                _itemContexts.Remove(cell);
+            }
+        }
+    }
+
+    enum ItemType
+    {
+        TopPadding,
+        BottomPadding,
+        Header,
+        Footer
+    }
+
+    class TypedItemContext
+    {
+        public TypedItemContext(VisualElement element, ItemType type)
+        {
+            Element = element;
+            Type = type;
+        }
+        public VisualElement Element { get; set; }
+        public ItemType Type { get; set; }
+    }
+
+    class ListViewItemContext : Xamarin.Forms.Platform.Tizen.Native.ListView.ItemContext
+    {
+        public ListViewItemContext(Cell cell, GroupList group, bool isGroup)
+        {
+            Cell = cell;
+            Group = group;
+            IsGroupItem = isGroup;
+        }
+        public ListViewItemContext(Cell cell, GroupList group) : this(cell, group, false)
+        {
+        }
+
+        public ListViewItemContext(GroupList group) : this(null, group, true)
+        {
+        }
+
+        public GroupList Group { get; set; }
     }
 }
