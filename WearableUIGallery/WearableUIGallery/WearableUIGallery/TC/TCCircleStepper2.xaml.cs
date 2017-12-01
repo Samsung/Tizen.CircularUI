ed131997b4bb06b1ab38eeea94dc652b0d68391c
Hyunjin <hj.na.park@samsung.com>
2017-12-01 10:10:21 +0900

    [CircleStepper] add TC for AbsolutLayout and modify TC for Grid

---
+++ WearableUIGallery/WearableUIGallery/WearableUIGallery/TC/TCCircleStepper2.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.CircularUI;
+using Xamarin.Forms.Xaml;
+
+namespace WearableUIGallery.TC
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TCCircleStepper2 : CirclePage
+    {
+		public TCCircleStepper2()
+		{
+            InitializeComponent ();
+		}
+
+        void OnFocusedHr(object sender, ValueChangedEventArgs args)
+        {
+            RotaryFocusTargetName = "StepperHr2";
+        }
+        void OnFocusedMm(object sender, ValueChangedEventArgs args)
+        {
+            RotaryFocusTargetName = "StepperMm2";
+        }
+        void OnFocusedSec(object sender, ValueChangedEventArgs args)
+        {
+            RotaryFocusTargetName = "StepperSec2";
+        }
+
+    }
+}
