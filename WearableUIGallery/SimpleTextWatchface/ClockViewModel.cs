e64701457a09a3e78f31c00cbe5899d8ec4820d7
이비오/Tizen Platform Lab(SR)/Engineer/삼성전자 <pius.lee@samsung.com>
2018-02-07 17:22:30 +0900

    Add lifecycle managing class for Watchface. (#77)
    - Add lifecycle managing class for Watchface.
     - Add Test Sample for Watchface class
     - Update xamarin.forms.platform.tizen version

---
+++ WearableUIGallery/SimpleTextWatchface/ClockViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace SimpleTextWatchface
+{
+    public class ClockViewModel : INotifyPropertyChanged
+    {
+        DateTime _time;
+
+        public DateTime Time
+        {
+            get => _time;
+            set
+            {
+                if (_time != value) return;
+                _time = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
