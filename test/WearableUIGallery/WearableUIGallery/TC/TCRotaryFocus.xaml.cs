ad40d093be4f531716ff9728ea89380fad03875c
pius lee <idkiller@gmail.com>
2018-06-25 14:15:39 +0900

    Add Test for RotaryFocus Properties (#52)
    TCRotaryFocus test switching RotaryFocusObject and
    RotaryFocusTargetName.

---
+++ test/WearableUIGallery/WearableUIGallery/TC/TCRotaryFocus.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tizen.Wearable.CircularUI.Forms;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WearableUIGallery.TC
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TCRotaryFocus : CirclePage
+	{
+        TapGestureRecognizer _universeClicked;
+        TapGestureRecognizer _spacemanClicked;
+        TapGestureRecognizer _sliderClicked;
+
+        public TCRotaryFocus ()
+		{
+			InitializeComponent ();
+
+            var universe = new RotaryFocusProxy(Universe);
+
+            RotaryFocusObject = universe;
+
+            _universeClicked = new TapGestureRecognizer();
+            _universeClicked.Command = new Command(() => RotaryFocusObject = universe);
+
+            _spacemanClicked = new TapGestureRecognizer();
+            _spacemanClicked.Command = new Command(() => RotaryFocusTargetName = "Spaceman");
+
+            _sliderClicked = new TapGestureRecognizer()
+            {
+                Command = new Command(() => RotaryFocusTargetName = "Slider")
+            };
+
+            Universe.GestureRecognizers.Add(_universeClicked);
+            Spaceman.GestureRecognizers.Add(_spacemanClicked);
+            SliderTarget.GestureRecognizers.Add(_sliderClicked);
+		}
+	}
+
+    class RotaryFocusImage : Image, IRotaryEventReceiver
+    {
+        public void Rotate(RotaryEventArgs args) => this.RelRotateTo(args.IsClockwise ? 30 : -30);
+    }
+
+    class RotaryFocusProxy : IRotaryEventReceiver
+    {
+        Image _img;
+        public RotaryFocusProxy(Image img)
+        {
+            _img = img;
+        }
+
+        public void Rotate(RotaryEventArgs args) => _img.RelRotateTo(args.IsClockwise ? 30 : -30);
+    }
+}
