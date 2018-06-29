44783095daffeda98ddbe1c5b697a053ef0efcca
pius lee <idkiller@gmail.com>
2018-06-29 19:36:48 +0900

    Add Tcs for CircleProgressBar and ActionButtonItem (#59)
    Add TC for ActionButtonItem properties
    Add TC for CircleProgressBarSurfaceItem.Value

---
+++ test/WearableUIGallery/WearableUIGallery/TC/TCCircleProgressBarSurfaceItem.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tizen.Wearable.CircularUI.Forms;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WearableUIGallery.TC
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TCCircleProgressBarSurfaceItem : CirclePage
+	{
+        bool _timerFinished;
+        double _increment = 0.02;
+
+		public TCCircleProgressBarSurfaceItem ()
+		{
+			InitializeComponent ();
+
+            ProgressBar.Value = 0;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _timerFinished = false;
+            Device.StartTimer(TimeSpan.FromMilliseconds(200), () =>
+            {
+                ProgressBar.Value += _increment;
+                if (ProgressBar.Value == 0 || ProgressBar.Value == 1) _increment *= -1;
+                return !_timerFinished;
+            });
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            _timerFinished = true;
+        }
+    }
+}
