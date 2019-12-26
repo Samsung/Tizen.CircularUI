86e87e7bf7ba255d9a010f1c184b0245ec384f0a
Seungkeun Lee <sngn.lee@samsung.com>
2019-12-26 10:13:14 +0900

    Add Shell navigation example

---
+++ sample/ShellExamples/ShellNavigation/SubPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Tizen.Wearable.CircularUI.Forms;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShellNavigation
+{
+    [QueryProperty("From", "from")]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SubPage : CirclePage
+	{
+		public SubPage ()
+		{
+			InitializeComponent ();
+            BindingContext = this;
+        }
+
+        string _from;
+        public string From
+        {
+            get
+            {
+                return _from;
+            }
+            set
+            {
+                _from = Uri.UnescapeDataString(value);
+                OnPropertyChanged("From");
+            }
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                Current.Text = $"Current : {Shell.Current?.CurrentState?.Location}";
+            });
+        }
+
+        public Command OnMain { get; } = new Command(async () =>
+        {
+            await Shell.Current.GoToAsync("//Main");
+        });
+        public Command OnSetting { get; } = new Command(async () =>
+        {
+            await Shell.Current.GoToAsync("//Setting");
+        });
+
+        async void OnClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.Navigation.PopAsync();
+        }
+    }
+}
