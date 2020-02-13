2afbc75626cab32616a502c6924bd1c7d11669c9
Seungkeun Lee <sngn.lee@samsung.com>
2020-02-13 10:13:16 +0900

    Add CircularShell Test app

---
+++ test/CircularShellGallery/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Tizen.Wearable.CircularUI.Forms;
+namespace CircularShellGallery
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainPage : CirclePage
+	{
+		public MainPage ()
+		{
+			InitializeComponent();
+            TestCases.ItemTapped += OnItemTapped;
+        }
+
+        void OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            Shell.Current.GoToAsync($"//{(e.Item as string)}");
+        }
+    }
+}
