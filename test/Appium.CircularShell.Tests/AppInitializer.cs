a208d45390c343d57cf157c00cd97d2d8f701496
Seungkeun Lee <sngn.lee@samsung.com>
2020-02-26 11:18:47 +0900

    Add appium test for CircularShell (#241)
    * Add Appium unit testcase
    
    * Update Shell UI Tests (#236)
    
    Co-authored-by: shmin <shmin.dev@gmail.com>

---
+++ test/Appium.CircularShell.Tests/AppInitializer.cs
@@ -0,0 +1,17 @@
+﻿namespace Appium.CircularShell.Tests
+{
+    public class AppInitializer
+    {
+        static TizenDriverApp s_instance;
+        public static TizenDriverApp GetDriver()
+        {
+            if (s_instance == null)
+            {
+                //string address = "http://10.113.165.80:4723/wd/hub";
+                string address = "http://127.0.0.1:4723/wd/hub";
+                s_instance = TizenDriverApp.CreateDriver(address, "emulator-26101", "org.tizen.example.CircularShellGallery");
+            }
+            return s_instance;
+        }
+    }
+}
---
+++ test/Appium.CircularShell.Tests/TC/TCFlyoutBackgroundColor.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System.Threading;
+
+
+namespace Appium.CircularShell.Tests
+{
+    [TestFixture]
+    public class TCFlyoutBackgroundColor : TestCaseBase
+    {
+        public TCFlyoutBackgroundColor() : base("FlyoutBackgroundColor")
+        {
+        }
+
+        [Test]
+        public void SetBlueViolet()
+        {
+            FindAndClick("BlueViolet");
+            FindAndClick("Open");
+            GoBack();
+            FindAndClick("default");
+        }
+
+        [Test]
+        public void SetBrown()
+        {
+            FindAndClick("Brown");
+            FindAndClick("Open");
+            GoBack();
+            FindAndClick("default");
+        }
+
+        [Test]
+        public void SetFuchsia()
+        {
+            FindAndClick("Fuchsia");
+            FindAndClick("Open");
+            GoBack();
+            FindAndClick("default");
+        }
+
+    }
+}
