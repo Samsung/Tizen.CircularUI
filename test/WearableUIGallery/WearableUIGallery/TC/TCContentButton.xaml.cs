0369e53b2a1deac18b49677eacdf4aecf0993c91
shmin <shyun.min@samsung.com>
2020-04-01 17:18:20 +0900

    Add new ContentButton (#263)
    * Add new ContentButton
    
    * Change Renderer inheritance and fix typo
    
    * Restructure ContentButton and ContentButtonRenderer
    
    * Fix layout issue
    
    * Remove OnLayoutUpdate method
    
    * Fix typo

---
+++ test/WearableUIGallery/WearableUIGallery/TC/TCContentButton.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WearableUIGallery.TC
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ContentButtonTestPage : ContentPage
+    {
+        public ContentButtonTestPage()
+        {
+            InitializeComponent();
+
+            ClickCommand = new Command(execute: () =>
+            {
+                label.Text = "clicked";
+            });
+        }
+
+        public ICommand ClickCommand { get; private set; }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine($"ContentButton clicked event is invoked!!");
+        }
+
+        private void OnButtonPressed(object sender, EventArgs e)
+        {
+            Xamarin.Forms.PlatformConfiguration.TizenSpecific.Image.SetBlendColor(buttonBg, Color.Gray);
+        }
+
+        private void OnButtonReleased(object sender, EventArgs e)
+        {
+            Xamarin.Forms.PlatformConfiguration.TizenSpecific.Image.SetBlendColor(buttonBg, Color.Transparent);
+        }
+    }
+}
