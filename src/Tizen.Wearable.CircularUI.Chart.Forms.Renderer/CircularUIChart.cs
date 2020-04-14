570c566278bd8ed948f0a7f36aa59b01d82b77ec
Kangho Hur <kangho.hur@samsung.com>
2020-04-14 11:02:30 +0900

    Adds CircularUI.Chart

---
+++ src/Tizen.Wearable.CircularUI.Chart.Forms.Renderer/CircularUIChart.cs
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2018 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Flora License, Version 1.1 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://floralicense.org/license/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Tizen.Wearable.CircularUI.Chart.Forms
+{
+    public static class FormsCircularUIChart
+    {
+        public static readonly string Tag = "CircularUI.Chart";
+
+        public static bool IsInitialized { get; private set; }
+
+        public static void Init()
+        {
+            if (IsInitialized) return;
+            IsInitialized = true;
+        }
+    }
+}
