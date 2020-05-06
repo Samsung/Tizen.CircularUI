004ee9fc6dad300a89c40d993e858b43cf4794eb
Seungkeun Lee <sngn.lee@samsung.com>
2020-05-06 19:29:33 +0900

    Refactoring CircularShell Renderer

---
+++ src/Tizen.Wearable.CircularUI.Forms.Renderer/Shell/CircularShellRenderer.cs
@@ -0,0 +1,76 @@
+﻿using ElmSharp;
+using System;
+using System.Linq;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Tizen;
+using XShell = Xamarin.Forms.Shell;
+
+[assembly: ExportRenderer(typeof(XShell), typeof(Tizen.Wearable.CircularUI.Forms.Renderer.CircularShellRenderer))]
+namespace Tizen.Wearable.CircularUI.Forms.Renderer
+{
+    public class CircularShellRenderer : Xamarin.Forms.Platform.Tizen.Watch.ShellRenderer
+    {
+
+        NavigationDrawer _drawer;
+        NavigationView _navigationView;
+
+        public CircularShellRenderer()
+        {
+            RegisterPropertyHandler(CircularShell.FlyoutIconBackgroundColorProperty, UpdateFlyoutIconBackgroundColor);
+            RegisterPropertyHandler(CircularShell.FlyoutForegroundColorProperty, UpdateFlyoutForegroundColor);
+        }
+
+        protected override NavigationDrawer CreateNavigationDrawer(EvasObject parent)
+        {
+            return _drawer = base.CreateNavigationDrawer(EvasObject parent);
+        }
+
+        protected override NavigationView CreateNavigationView(EvasObject parent)
+        {
+            return _navigationView = base.CreateNavigationView(parent);
+        }
+
+        protected override void OnNavigationDrawerToggled(object sender, EventArgs e)
+        {
+            base.OnNavigationDrawerToggled(sender, e);
+            if (!Element.FlyoutIsPresented)
+            {
+                var currentPage = GetCurrentPage(Element);
+
+                if (currentPage != null)
+                {
+                    var renderer = Platform.GetOrCreateRenderer(currentPage);
+                    (renderer as CirclePageRenderer)?.UpdateRotaryFocusObject(false);
+                }
+            }
+        }
+
+        void UpdateFlyoutIconBackgroundColor()
+        {
+            _drawer.HandlerBackgroundColor = CircularShell.GetFlyoutIconBackgroundColor(Element).ToNative();
+        }
+
+        void UpdateFlyoutForegroundColor(bool init)
+        {
+            if (init && CircularShell.GetFlyoutForegroundColor(Element).IsDefault)
+                return;
+
+            if (_navigationView != null)
+            {
+                _navigationView.ForegroundColor = CircularShell.GetFlyoutForegroundColor(Element).ToNative();
+            }
+        }
+
+        static Page GetCurrentPage(XShell shell)
+        {
+            var stack = (shell.CurrentItem.CurrentItem as ShellSection)?.Stack;
+            var currentPage = stack?.LastOrDefault<Page>();
+
+            if (currentPage == null)
+            {
+                currentPage = (shell.CurrentItem.CurrentItem.CurrentItem as IShellContentController)?.Page;
+            }
+            return currentPage;
+        }
+    }
+}
