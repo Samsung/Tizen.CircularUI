a72897691260495032123fc136bb9343cabe2178
Seungkeun Lee <sngn.lee@samsung.com>
2020-05-13 19:23:09 +0900

    Update Bezel Intercation behavior on closing dialog (#304)

---
+++ src/Tizen.Wearable.CircularUI.Forms.Renderer/DatePickerRenderer.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Forms;
+
+using XDatePickerRenderer = Xamarin.Forms.Platform.Tizen.DatePickerRenderer;
+using CDatePickerRenderer = Tizen.Wearable.CircularUI.Forms.Renderer.DatePickerRenderer;
+using System;
+
+[assembly: ExportRenderer(typeof(DatePicker), typeof(CDatePickerRenderer))]
+
+namespace Tizen.Wearable.CircularUI.Forms.Renderer
+{
+    public class DatePickerRenderer : XDatePickerRenderer
+    {
+        protected override void OnPickerOpened(object sender, EventArgs args)
+        {
+            Element.FindBezelController()?.Deactivate();
+            base.OnPickerOpened(sender, args);
+        }
+
+        protected override void OnPickerClosed(object sender, EventArgs args)
+        {
+            base.OnPickerClosed(sender, args);
+            Element.FindBezelController()?.Activate();
+        }
+    }
+}
