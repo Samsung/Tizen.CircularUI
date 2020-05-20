f8c5ff8dd1b5f47ff2af97bc63bbfdb2547287d4
Kangho Hur <kangho.hur@samsung.com>
2020-05-20 19:11:41 +0900

    Add TC for check minsize

---
+++ test/WearableUIGallery/WearableUIGallery/TC/TCCheckListView.xaml.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Copyright (c) 2018 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Flora License, Version 1.1 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://floralicense.org/license/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Tizen.Wearable.CircularUI.Forms;
+
+namespace WearableUIGallery.TC
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TCCheckListView : ContentPage
+    {
+        public TCCheckListView()
+        {
+            InitializeComponent();
+        }
+
+        public void OnSelected(object sender, CheckedChangedEventArgs args)
+        {
+            Check check = sender as Check;
+            if (check != null)
+            {
+                Console.WriteLine($"<<OnSelected>>  args.Value:{args.Value}");
+                Console.WriteLine($"<<OnSelected>>  Radio IsChecked:{check.IsToggled}");
+            }
+        }
+    }
+}
